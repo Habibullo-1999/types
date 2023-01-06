f6699f2767f44a27b0ae04de13959122aea4081d
Habibullo-1999 <habibullo.boss@gmail.com>
2023-01-06 17:57:57 +0500

    добавлеени типи

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,30 @@
+package types
+
+// Money представляет собой денежную единицу в минимальных единицах
+// (центы, копейки, дирамы и т.д.)
+type Money int64
+
+// Currency представляет код валюты
+type Currency string
+
+// Коды валют
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+// PAN представляет номер карты
+type PAN string
+
+// Card предствляет информацию о платежной карте
+type Card struct {
+	ID       int
+	PAN      PAN
+	Balance  Money // Used Money type
+	MinBalance Money
+	Currency Currency
+	Color    string
+	Name     string
+	Active   bool
+}
